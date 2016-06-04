eef84c5e895b53376db925709351fbfa8a9b03c2
Ger Hobbelt <ger@hobbelt.com>
2016-06-04 19:56:54 +0200

    whitespace

--- event.drag/jquery.event.drag.js
+++ event.drag/jquery.event.drag.js
@@ -418,7 +418,7 @@ var drag = $special.drag = {
         return obj;
     },
 
-	// Determine if the argument is an element or jquery instance 
+    // Determine if the argument is an element or jquery instance 
     // and only pass it through when it is, otherwise produce `undefined` instead
     element: function ( arg ) {
         if ( arg && ( arg.jquery || arg.nodeType === 1 ) ) {
